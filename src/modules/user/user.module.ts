f33373f8cc613a2b6e99a496d14a21d565371840
タイラー <tylerymh27@gmail.com>
2023-07-04 22:01:02 -0400

    Add: user auth with jwt

---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UsersController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './jwt.constants';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: { expiresIn: '20h' },
+        }),
+    ],
+    controllers: [UsersController],
+    providers: [UserService, JwtStrategy],
+})
+export class UserModule { }
